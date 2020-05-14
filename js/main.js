9a130e65c868bb41c3506d951ebddcdf124d0c28
Fernando <fernando_lopezc@live.com>
2020-05-14 11:09:48 +0100

    configure options for notifcation

--- js/main.js
+++ js/main.js
@@ -29,13 +29,33 @@ if (!('Notification' in window)) {
 }
   // TODO 2.2 - request permission to show notifications
   Notification.requestPermission(status => console.log("Notification permission status:",status))
-
+ // TODO 2.3 - display a Notification
   function displayNotification() {
+   if (Notification.permission == 'granted') {
+  navigator.serviceWorker.getRegistration().then(reg => {
 
-    // TODO 2.3 - display a Notification
+    // TODO 2.4 - Add 'options' object to configure the notification
+    const options = {
+  body: 'First notification!',
+  icon: 'images/notification-flat.png',
+  vibrate: [100, 50, 100],
+  data: {
+    dateOfArrival: Date.now(),
+    primaryKey: 1
+  },
 
+  // TODO 2.5 - add actions to the notification
+
+  // TODO 5.1 - add a tag to the notification
+
+};
+    reg.showNotification("Hello world",options);
+  });
+}
   }
-
+ notifyButton.addEventListener('click', () => {
+    displayNotification();
+  });
   function initializeUI() {
 
     // TODO 3.3b - add a click event listener to the "Enable Push" button
@@ -105,9 +125,7 @@ if (!('Notification' in window)) {
     return outputArray;
   }
 
-  notifyButton.addEventListener('click', () => {
-    displayNotification();
-  });
+ 
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
